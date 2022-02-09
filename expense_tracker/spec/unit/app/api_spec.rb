8a885ce57b45946dbeb40871a22fcb20d99deb2e
juanlopezsilva <97121231+juanlopezsilva@users.noreply.github.com>
2022-02-09 12:21:24 -0300

    Develop (#4)
    * feat: add expenses structure endpoint api
    
    * feat: add spec helper configuration
    
    * feat: add rack app configuration
    
    * test: add pending acceptance spec
    
    * feat: add filter gem from backtrace conf
    
    * Chapter 5: Unit Specs (#2)
    
    * feat: add ledger initialize in api
    
    * test: add post expenses successfully recorded spec
    
    * refactor: use a hook for duplicate code in api spec
    
    * feat: add handling failure in post expenses endpoint
    
    * test: add fails validation post expenses spec
    
    * feat: add ledger class
    
    * refactor: add parsed response body helper method
    
    * feat: add expenses_on method on ledger
    
    * feat: call expenses_on method in get expenses endpoint
    
    * test: add get expenses spec with date param
    
    * Chapter 6: Integration Specs (#3)
    
    * feat: add sequel database toolkit
    
    * feat: add expense migration
    
    * feat: add db configuration
    
    * feat: add record logic ledger method
    
    * feat: add db tag expense_tracker_api_spec
    
    * test: add record method ledger spec
    
    * feat: add expenses_on logic ledger method
    
    * test: add expenses_on method ledger spec
    
    * test: erase pending status expense_tracker_api_spec

---
+++ expense_tracker/spec/unit/app/api_spec.rb
@@ -0,0 +1,95 @@
+require_relative '../../../app/api'
+require 'rack/test'
+
+module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
+  RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    def parsed_response_body
+      JSON.parse(last_response.body)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    describe 'GET /expenses/:date' do
+      context 'when expenses exist on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2017-06-12')
+            .and_return({ 'some' => 'data' })
+        end
+
+        it 'returns the expense records as JSON' do
+          get '/expenses/2017-06-12'
+          expect(parsed_response_body).to eq({ 'some' => 'data' })
+        end
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-12'
+          expect(last_response.status).to eq(200)
+        end
+      end
+      context 'when there are no expenses on the given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2017-06-12')
+            .and_return([])
+        end
+        it 'returns an empty array as JSON' do
+          get '/expenses/2017-06-12'
+          expect(parsed_response_body).to eq([])
+        end
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-12'
+          expect(last_response.status).to eq(200)
+        end
+      end
+    end
+
+    describe 'POST /expenses' do
+      context 'when the expense is successfully recorded' do
+        let(:expense) { { 'some' => 'data' } }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+        end
+
+        it 'returns the expense id' do
+          post '/expenses', JSON.generate(expense)
+          expect(parsed_response_body).to include('expense_id' => 417)
+        end
+
+        it 'responds with a 200 (OK)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the expense fails validation' do
+        let(:expense) { { 'some' => 'data' } }
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+          expect(parsed_response_body).to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(422)
+        end
+      end
+    end
+  end
+end
